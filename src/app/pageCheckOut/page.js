49ece6f0c1017a7209e78702acc7195bf7cdb1e9
adityaaryas <144906564+adityaaryas@users.noreply.github.com>
2024-06-15 04:10:51 +0700

    Update page.js

--- src/app/pageCheckOut/page.js
+++ src/app/pageCheckOut/page.js
@@ -1,14 +1,28 @@
 "use client";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import CheckOut from "@/components/CheckOut";
 
 export default function page() {
   return (
-    <>
+    <div className="bg-[#DAD3BE] scroll-smooth focus:scroll-auto">
       <div>
         <Header />
       </div>
-      <p>HALOO</p>
-    </>
+      <div className="font-bold font-sans text-5xl px-20 h-60 flex items-center">
+        Checkout
+      </div>
+      <div className="font-semibold font-sans text-3xl px-20 mb-20 flex items-center justify-between">
+        Keranjangmu
+        <div className="font-semibold font-sans text 3x1 px-4 text-left">
+        Pembayaran
+      </div>
+      </div>
+      <div className="">
+        <CheckOut />
+      </div>
+      <Footer />
+     </div>
   );
 }
